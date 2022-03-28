96179d323dd0b577af024ea7257ccaa960262723
pajgo <pavel.rogovoi@gmail.com>
2022-03-28 18:53:20 +0600

    fix: image generation

--- .mdeprc.js
+++ .mdeprc.js
@@ -1,8 +1,11 @@
 const dbSrv = process.env.DB_SRV || "redisSentinel";
+const repo = process.env.DOCKER_REPO || 'makeomatic/ms-users-haproxy';
+const node = '16';
 
 exports = module.exports = {
-  node: '16',
+  node,
   auto_compose: true,
+  repo,
   services: [],
   with_local_compose: true,
   nycCoverage: false,
@@ -20,7 +23,11 @@ exports = module.exports = {
         NCONF_NAMESPACE: 'MS_USERS_TOKEN_SERVER',
       },
     },
-  }
+  },
+  dba: {
+    HAPROXY_IMAGE_NAME: `${repo}/ms-users-haproxy`,
+    HAPROXY_IMAGE_VERSION: `${node}-${process.env.npm_package_version}`
+  },
 };
 
 
